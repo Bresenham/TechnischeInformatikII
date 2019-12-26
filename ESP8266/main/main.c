dd5b940942ad3cdee23e6b676bc758f83f4fe978
Bresenham <gaul@hm.edu>
2019-12-26 01:25:43 +0100

    Update for today

--- ESP8266/main/main.c
+++ ESP8266/main/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
 #include "freertos/task.h"
 
 #include "esp_system.h"
@@ -11,34 +12,41 @@
 #include "driver/gpio.h"
 #include "driver/spi.h"
 
-#define	LED_OUTPUT_PIN_SEL	( (1 << GPIO_NUM_2) | (1 << GPIO_NUM_2) )
+#define	LED_OUTPUT_PIN_SEL	(1 << GPIO_NUM_2)
 
 #define READ_CMD			0x13
 #define WRITE_CMD			0x12
 
 bool isLEDon = false;
+SemaphoreHandle_t mutex;
+TaskHandle_t spi_task_handle;
 
-void send_spi(spi_trans_t *spi_trans, uint8_t *data, uint8_t length) {
-	memset(trans, 0, sizeof(*trans));
-	
-	spi_trans->bits.mosi = 8 * length;
-	spi_trans->mosi = data;
-	spi_trans->addr = NULL;
-	spi_trans->cmd = NULL;
+void send_spi(spi_trans_t *trans, uint8_t *data, uint8_t length) {
+  memset(trans, 0, sizeof(*trans));
+  
+  trans->bits.mosi = length * 8;
+  trans->mosi = data;
+  trans->addr = NULL;
+  trans->cmd = NULL;
 
-	spi_trans(HSPI_HOST, spi_trans);
+  spi_trans(HSPI_HOST, trans);
 }
 
-void read_spi(spi_trans_t *trans, uint8_t *dest) {
-	memset(trans, 0, sizeof(*trans));
-	
-	trans->bits.miso = 8;
-	spi_trans->miso = dest;
-	spi_trans->addr = NULL;
-	spi_trans->cmd = NULL;
-	
-	spi_trans(HSPI_HOST, spi_trans);
+void read_spi(spi_trans_t *trans, uint8_t *buf) {
+  memset(trans, 0, sizeof(*trans));
+  
+  trans->bits.miso = 8;
+  trans->miso = buf;
+  trans->addr = NULL;
+  trans->cmd = NULL;
+  
+  spi_trans(HSPI_HOST, trans);
 }
+
+uint8_t readMsgBuf[4];
+uint8_t writeMsgBuf[5];
+uint8_t recvBuf[1];
+spi_trans_t trans;
 
 typedef enum STATE {
 	IDLE = 0,
@@ -54,75 +62,99 @@ void ICACHE_FLASH_ATTR spi_master_write_slave_task(void *arg) {
 	const uint8_t addrMSB =  (addr >> 16);
 	const uint8_t addrMLSB = (addr >> 8);
 	const uint8_t addrLSB = (addr & 0xFF);
-	
-	uint8_t readMsg[] = {
-		READ_CMD, addrMSB, addrMLSB, addrLSB
-	};
-	
-	uint8_t writeMsg[] = {
-		WRITE_CMD, addrMSB, addrMLSB, addrLSB, 133
-	};
-	
-	uint8_t recvBuff[1];
-	
-	spi_trans_t spi_trans;
+
+	readMsgBuf[0] = READ_CMD;
+	readMsgBuf[1] = addrMSB;
+	readMsgBuf[2] = addrMLSB;
+	readMsgBuf[3] = addrLSB;
+
+	writeMsgBuf[0] = WRITE_CMD;
+	writeMsgBuf[1] = addrMSB;
+	writeMsgBuf[2] = addrMLSB;
+	writeMsgBuf[3] = addrLSB;
+	writeMsgBuf[4] = 133;
 
 	while(true) {
-
 		switch(state) {
-			case IDLE:
-				send_spi(&spi_trans, writeMsg, 5);
+			case IDLE: {
+				send_spi(&trans, writeMsgBuf, sizeof(writeMsgBuf));
+
+				xSemaphoreTake(mutex, portMAX_DELAY);
+				//vTaskSuspend(spi_task_handle);
+
 				state = SENT_WRITE;
+			}
 			break;
-			case SENT_WRITE:
-				send_spi(&spi_trans, readMsg, 4);
+
+			case SENT_WRITE: {
+				send_spi(&trans, readMsgBuf, sizeof(readMsgBuf));
+
+				xSemaphoreTake(mutex, portMAX_DELAY);
+				//vTaskSuspend(spi_task_handle);
+
 				state = SENT_READ;
+			}
 			break;
-			default:
-				;
-				const uint8_t resp = read_spi(&spi_trans, recvBuff);
-				printf("RESPONSE: %d\n", resp);
+
+			case SENT_READ: {
+				read_spi(&trans, recvBuf);
+
 				state = IDLE;
+			}
 			break;
-		}
-
-		if(isLEDon) {
-			gpio_set_level(GPIO_NUM_2, 1);
-		} else {
-			gpio_set_level(GPIO_NUM_2, 0);
-		}
-		isLEDon = !isLEDon;
-
-		vTaskDelay(10 / portTICK_PERIOD_MS);
 	}
+
+	if(isLEDon) {
+		gpio_set_level(GPIO_NUM_2, 1);
+		isLEDon = false;
+	} else {
+		gpio_set_level(GPIO_NUM_2, 0);
+		isLEDon = true;
+	}
+
+	vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+void spi_callback(int event, void *arg) {
+    switch (event) {
+        case SPI_INIT_EVENT: {
+			xTaskCreate(spi_master_write_slave_task, "spi_master_write_slave", 2048, NULL, 3, &spi_task_handle);
+        }
+        break;
+
+        case SPI_TRANS_START_EVENT: {
+        }
+        break;
+
+        case SPI_TRANS_DONE_EVENT: {
+			xSemaphoreGive(mutex);
+			//xTaskResumeFromISR(spi_task_handle);
+        }
+        break;
+
+        case SPI_DEINIT_EVENT: {
+        }
+        break;
+    }
 }
 
 void ICACHE_FLASH_ATTR app_main() {
+	mutex = xSemaphoreCreateMutex();
+	
 	gpio_config_t io_conf;
+	io_conf.intr_type = GPIO_INTR_DISABLE;
 	io_conf.mode = GPIO_MODE_OUTPUT;
 	io_conf.pin_bit_mask = LED_OUTPUT_PIN_SEL;
 	io_conf.pull_down_en = 0;
 	io_conf.pull_up_en = 0;
 	gpio_config(&io_conf);
-	
+  
 	spi_config_t spi_config;
 	spi_config.interface.val = SPI_DEFAULT_INTERFACE;
+	spi_config.intr_enable.val = SPI_MASTER_DEFAULT_INTR_ENABLE;
 	spi_config.mode = SPI_MASTER_MODE;
 	spi_config.clk_div = SPI_2MHz_DIV;
-	spi_config.event_cb = NULL;
+	spi_config.event_cb = &spi_callback;
 	spi_init(HSPI_HOST, &spi_config);
-	
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP8266 chip with %d CPU cores, WiFi, ",
-            chip_info.cores);
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-			
-	xTaskCreate(spi_master_write_slave_task, "spi_master_write_slave", 2048, NULL, 3, NULL);
 }
